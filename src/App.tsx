996cffbebb4a47860e3605ecd93eac032e6a1593
Ken <kchau@microsoft.com>
2019-04-30 11:26:11 -0700

    adds office-ui-fabric-react to the create-react-app template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Stack, Text, Link, FontWeights } from 'office-ui-fabric-react';
 
-const App: React.FC = () => {
+const boldStyle = { root: { fontWeight: FontWeights.semibold } };
+
+export const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack
+      horizontalAlign="center"
+      verticalAlign="center"
+      verticalFill
+      styles={{
+        root: {
+          width: '960px',
+          margin: '0 auto',
+          textAlign: 'center',
+          color: '#605e5c'
+        }
+      }}
+      gap={15}
+    >
+      <img
+        src="https://raw.githubusercontent.com/Microsoft/just/master/packages/just-stack-uifabric/template/src/components/fabric.png"
+        alt="logo"
+      />
+      <Text variant="xxLarge" styles={boldStyle}>
+        Welcome to Your UI Fabric App
+      </Text>
+      <Text variant="large">For a guide on how to customize this project, check out the UI Fabric documentation.</Text>
+      <Text variant="large" styles={boldStyle}>
+        Essential Links
+      </Text>
+      <Stack horizontal gap={15} horizontalAlign="center">
+        <Link href="https://developer.microsoft.com/en-us/fabric">Docs</Link>
+        <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">Stack Overflow</Link>
+        <Link href="https://github.com/officeDev/office-ui-fabric-react/">Github</Link>
+        <Link href="https://twitter.com/officeuifabric">Twitter</Link>
+      </Stack>
+      <Text variant="large" styles={boldStyle}>
+        Design System
+      </Text>
+      <Stack horizontal gap={15} horizontalAlign="center">
+        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/icons">Icons</Link>
+        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/typography">Typography</Link>
+        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/themegenerator">Theme</Link>
+      </Stack>
+    </Stack>
   );
-}
-
-export default App;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { App } from './App';
+import { FluentCustomizations } from '@uifabric/fluent-theme';
+import { Customizer, mergeStyles } from 'office-ui-fabric-react';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Inject some global styles
+mergeStyles({
+  selectors: {
+    ':global(body), :global(html), :global(#root)': {
+      margin: 0,
+      padding: 0,
+      height: '100vh'
+    }
+  }
+});
+
+ReactDOM.render(
+  <Customizer {...FluentCustomizations}>
+    <App />
+  </Customizer>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
